a194ce293917e3aedae85273155e95c505fbdc08
alexey.samsonov <alexey.samsonov@myoffice.team>
2020-09-16 23:03:14 +0300

    try prime ui

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { SagaScheduler } from './components/main';
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 import './App.css';
 
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: 'auto',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
-
 function App() {
-
-  const classes = useStyles();
   return (
     <div className="App">
-      <SagaScheduler classes={classes}/>
+      <SagaScheduler/>
     </div>
   );
 }
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
-import { CharactersList } from './charactersList';
+
+import { Sidebar } from 'primereact/sidebar';
 
 
 
@@ -16,34 +13,46 @@ export class SagaScheduler extends React.Component {
 
   };
 
+  state = {
+      visible: true
+    };
+
   render() {
-    const classes = this.props.classes;
+    // const classes = this.props.classes;
+
+    // return (
+    //   <>
+    //     <AppBar position="fixed" className={classes.appBar}>
+    //       <Toolbar>
+    //         <Typography variant="h6" noWrap>
+    //           Saga Scheduler
+    //         </Typography>
+    //       </Toolbar>
+    //     </AppBar>
+    //     <Drawer
+    //       className={classes.drawer}
+    //       variant="permanent"
+    //       classes={{
+    //         paper: classes.drawerPaper,
+    //       }}
+    //     >
+    //     <Toolbar />
+    //       <div className={classes.drawerContainer}>
+    //         <CharactersList clickHandler={this.selectCharacter}/>
+    //       </div>
+    //     </Drawer>
+    //     <main className={classes.content}>
+    //       <Toolbar />
+    //     </main>
+    //   </>
+    // );
+
+    
 
     return (
-      <>
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" noWrap>
-              Saga Scheduler
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-        <Toolbar />
-          <div className={classes.drawerContainer}>
-            <CharactersList clickHandler={this.selectCharacter}/>
-          </div>
-        </Drawer>
-        <main className={classes.content}>
-          <Toolbar />
-        </main>
-      </>
+      <Sidebar visible={this.state.visible} onHide={() => this.setState({visible:false})}>
+        Content
+      </Sidebar>
     );
   }
 }
