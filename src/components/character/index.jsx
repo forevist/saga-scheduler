8f9c023e55a5d2cb7ff0c80b2041d1f8cc6ae8b0
alexey.samsonov <alexey.samsonov@myoffice.team>
2020-09-16 21:41:29 +0300

    add changing name

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -3,24 +3,72 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import EditIcon from '@material-ui/icons/Edit';
+import SaveIcon from '@material-ui/icons/Save';
+import CancelIcon from '@material-ui/icons/Cancel';
+import TextField from '@material-ui/core/TextField';
 
 
-export class CharacterName  extends React.Component {
-    selectCharacter = () => {
-      this.props.clickHandler(this.props.name);
-    };
+export class CharacterName extends React.Component {
+  state = {
+    edit: false,
+    newName: '',
+  };
 
-    editName = (e) => {
-      console.log('edit');
-      e.stopPropagation();
-    };
+  selectCharacter = () => {
+    this.props.clickHandler(this.props.name);
+  };
 
-    render() {
-      return (
-        <ListItem button onClick={this.selectCharacter}>
-          <ListItemText primary={this.props.name} />
-          <ListItemIcon onClick={this.editName}><EditIcon/></ListItemIcon>
-        </ListItem>
-      );
-    }
+  toggleEdit = (e) => {
+    this.setState({
+      edit: !this.state.edit,
+      newName: this.props.name,
+    });
+    e.stopPropagation();
+  };
+
+  renderSpan = () => {
+    return (
+      <>
+        <ListItemText primary={this.props.name} />
+        <ListItemIcon onClick={this.toggleEdit}><EditIcon/></ListItemIcon>
+      </>
+    );
+  }
+
+  changeCharacterName = (event) => {
+    this.setState({
+      newName: event.target.value
+    });
+  }
+
+  cancelEditingName = (e) => {
+    this.toggleEdit(e);
+  }
+
+  saveName = (e) => {
+    this.props.onChangeName(this.props.name, this.state.newName);
+    this.toggleEdit(e);
+  }
+
+  renderEdit = () => {
+    return (
+      <>
+        <TextField value={this.state.newName} onChange={this.changeCharacterName}/>
+        <ListItemIcon onClick={this.saveName}><SaveIcon/></ListItemIcon>
+        <ListItemIcon onClick={this.cancelEditingName}><CancelIcon/></ListItemIcon>
+      </>
+    );
+  }
+
+  render() {
+    const content = this.state.edit ? 
+      this.renderEdit() :
+      this.renderSpan();
+
+    return (
+      <ListItem button onClick={this.selectCharacter}>
+        {content}
+      </ListItem>
+    );
+  }
 };
--- src/components/charactersList/index.jsx
+++ src/components/charactersList/index.jsx
@@ -3,17 +3,32 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import { CharacterName } from '../character';
 
-export const CharactersList = (props) => {
 
-  const clickHandler = (characterName) => {
+export class CharactersList extends React.Component {
+  state = {
+    names: ['Test1', 'name2', 'name3'],
+  };
+
+  clickHandler = (characterName) => {
     console.log('characterName', characterName);
   }
+
+  onChangeName = (oldName, newName) => {
+    const newNames = [].concat(this.state.names);
+    newNames[newNames.indexOf(oldName)] = newName;
+    this.setState({
+      names: newNames,
+    })
+  }
       
-  return (
-    <List>
-      {['Test1', 'name2', 'name3'].map((name, index) => (
-        <CharacterName key={index} name={name} clickHandler={clickHandler}/>
-      ))}
-    </List>
-  );
+  render() {
+    const names = this.state.names
+    return (
+      <List>
+        {names.map((name, index) => (
+          <CharacterName key={index} name={name} clickHandler={this.clickHandler} onChangeName={this.onChangeName}/>
+        ))}
+      </List>
+    );
+  }
 };
