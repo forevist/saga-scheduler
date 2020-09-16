a549426757d06154bd19ee1fe3acf6796587e15d
alexey.samsonov <alexey.samsonov@myoffice.team>
2020-09-16 19:58:35 +0300

    Updates

---
+++ precache-manifest.0bead16c865385eb8e3c087b76497bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76844b54c37ae606a1d5fa58a0d8d74",
+    "url": "/saga-scheduler/index.html"
+  },
+  {
+    "revision": "2852297cd7f86a7137f7",
+    "url": "/saga-scheduler/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b81c068f1b3cefa3e1d",
+    "url": "/saga-scheduler/static/js/2.0d2f4147.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saga-scheduler/static/js/2.0d2f4147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2852297cd7f86a7137f7",
+    "url": "/saga-scheduler/static/js/main.c6a88d70.chunk.js"
+  },
+  {
+    "revision": "0bbde307d5676cec1c1c",
+    "url": "/saga-scheduler/static/js/runtime-main.9b7b0b02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/saga-scheduler/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93ec6918ce3aac3c29b166526a230abe.js"
+  "/saga-scheduler/precache-manifest.0bead16c865385eb8e3c087b76497bea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saga-scheduler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
